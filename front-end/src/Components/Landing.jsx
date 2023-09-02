1345366a0033e7db201a9ee48f41f076f264c305
Maushish <chamarnumber102@gmail.com>
2023-09-02 19:24:33 +0530

    Logos

--- front-end/src/Components/Landing.jsx
+++ front-end/src/Components/Landing.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {chatloom} from'/home/maushish/chatloom/front-end/public/Logo/chatloom.png'
+import {Group24} from '/home/maushish/chatloom/front-end/public/Logo/Group24.png'
 
 function Landing() {
   return (
     <div className='bg-mainBlack h-screen'>
       <div className=' text-txt1 flex flex-col items-center justify-center  '>
+          
           <h1 className=' mt-[20%] text-6xl '>with Security</h1>
           <div className=' text-5xl py-20 flex flex-col items-center justify-center' >
               <h2  >keep your identity  <span className='text-customGreen '> safe</span></h2>
