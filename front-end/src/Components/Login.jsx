0ce3cc1e90add1fc5f67d4225cdf31fdbe076770
Maushish <chamarnumber102@gmail.com>
2023-09-03 03:47:34 +0530

    Login Page text

--- front-end/src/Components/Login.jsx
+++ front-end/src/Components/Login.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
+    <div className='bg-mainBlack flex'>
+      <div className='mt-10  h-screen text-txt1'>
+        <h1 className=' px-10 text-5xl'>Set up your profile!</h1>
+        <h2 className='text-4xl px-10 py-5'>in <span className='text-customGreen'>2</span>simple steps</h2>
+      </div>
     </div>
 
     )
