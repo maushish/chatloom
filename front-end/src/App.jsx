6a0fcb21a14e37d79eb04fdf9832f07433e9c6f2
Maushish <chamarnumber102@gmail.com>
2023-09-02 19:53:49 +0530

    Router of Login page setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path='' element={<Landing/>}/>
-        <Route path='//' element={<Login/>}/>
+        <Route path='/Join' element={<Login/>}/>
         <Route path='/////' element={<Chat/>}/>
 
       </Routes>
--- front-end/src/Components/Landing.jsx
+++ front-end/src/Components/Landing.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import chatloom from '../Logo/chatloom.svg';
 import Group24 from '../Logo/Group24.svg';
 
-
 function Landing() {
   return (
     <div className='bg-mainBlack h-screen '>
@@ -19,7 +18,9 @@ function Landing() {
           <div className=' text-5xl py-20 flex flex-col items-center justify-center' >
               <h2>keep your identity <span className='text-customGreen '> safe</span></h2>
               <div className='py-20 text-black'>
+              <a href='/join'>
               <button className='px-4 py-2 bg-white rounded-md  hover:bg-black-gradient hover:text-white transition duration-950 hover:py-3 hover:px-5  border-2'>Join {'>'}</button>
+              </a>
               </div>
           </div>
       </div>
