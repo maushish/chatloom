334a94eab2990561885f76b242e4e5bd442633d3
Maushish <chamarnumber102@gmail.com>
2023-09-05 12:42:38 +0530

    Solved about bg bug

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   theme: {
     extend: {
       // Custom named colors
+      backgroundImage: {
+        'black-gradient': 'linear-gradient(280deg, rgba(45, 45, 45, 0.80) 0%, rgba(0, 0, 0, 0.85) 100%)',
+      },
       colors: {
         customGreen: '#5B9132',
         mainBlack: '#171515',
@@ -11,7 +14,6 @@ module.exports = {
       },
       backgroundColor:{
         'form': 'var(--grey-popup-base, #383838)',
-        'black-gradient': 'linear-gradient(280deg, rgba(45, 45, 45, 0.80) 0%, rgba(0, 0, 0, 0.85) 100%)',
       },
       //font-family
       fontFamily: {
