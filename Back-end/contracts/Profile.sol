af31287bf60b67f67ad43825f5beec5acfc9e1d3
maushish <chamarnumber102@gmail.com>
2023-09-05 13:29:24 +0530

    Some comments

--- Back-end/contracts/Profile.sol
+++ Back-end/contracts/Profile.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/// @title User-Profiles contract 
+/// @author Maushish
 contract user{
     struct profile{       
         string name;
         string Bio;
         string avatarHash;        
     }
+
     mapping (address=>profile) public userProfiles;
+
+    /// @notice Sets your profile with your name,bio and avatar image
+    /// @dev Better variabe names could be used for better understanding
     function setProfile(string memory _name, string memory _Bio, string memory _avatarHash) public {
         userProfiles[msg.sender]=profile(_name,_Bio,_avatarHash);
     }
