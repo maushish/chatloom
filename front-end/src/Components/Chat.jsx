308020abbf8c85293692e50ed77f3396becf2ad8
maushish <chamarnumber102@gmail.com>
2023-09-08 23:39:05 +0530

    Layout issue solved

--- front-end/src/Components/Chat.jsx
+++ front-end/src/Components/Chat.jsx
@@ -6,18 +6,18 @@ function Chat() {
   return (
     <div className='bg-mainBlack h-screen flex flex-col items-center justify-center'>
       {/* Whole chat thing */}
-      <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[60%] ml-[38%] mt-[3%] h-[92%]'>
+      <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[60%] ml-[38%] mt-[3%] h-[78%]'>
         <div>Name</div>
         <div>Chat</div>
         <div>emoji, typing</div>
       </div>
       {/* Profiles Section */}
-      <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[30%] mr-[60%] h-[50%] mt-[-54%] xl:mt-[-35%]'>
+      <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[30%] mr-[60%] h-[50%] mt-[-54%] xl:mt-[-29%]'>
         {/* USER'S PROFILE */}
         <div>
           <div className='w-[40vh] h-[8vh] bg-customGreen rounded-lg mt-[-100%]'>
             <div></div>
-            <div className='z=10 opacity-100'>
+            <div className='z=10'>
               <a>
                 <img src={convo} className='h-[8vh] ml-20'></img>
               </a>
