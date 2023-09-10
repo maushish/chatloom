e62de75a04cabd781b217f18f757015ba0cbb229
maushish <chamarnumber102@gmail.com>
2023-09-11 03:22:50 +0530

    Formated better design layout

--- front-end/src/Components/Chat.jsx
+++ front-end/src/Components/Chat.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import convo from '../Logo/chatting.svg';
-import pfp from '../Logo/pfp.svg';
-import { AiOutlineUserAdd} from 'react-icons/ai'
+import { AiOutlineUserAdd } from 'react-icons/ai';
 
 function Chat() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -22,7 +20,6 @@ function Chat() {
   };
 
   return (
-
     <div className='bg-mainBlack h-screen flex flex-col items-center justify-center'>
       {/* Whole chat thing */}
       <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[60%] ml-[38%] mt-[3%] h-[78%]'>
@@ -34,56 +31,58 @@ function Chat() {
       </div>
       {/* Profiles Section */}
       <div>
-        <div className='w-[40vh] h-[6vh] bg-customGreen rounded-lg mt-[-180%] ml-[-150%] flex flex-col items center justify-center'>
-          <div className='mt-[16%] ml-[-5%]'>
-          <div className="w-[80px] h-[80px] bg-gray-200 rounded-full overflow-hidden  mt-[3%]">
+        <div className='w-[20vh] h-[6vh] bg-customGreen rounded-lg mt-[-430%] ml-[-360%] flex flex-row items-center'>
+          {/* Left section with circular profile picture */}
+          <div className='w-[120px] h-[80px] bg-gray-200 rounded-full overflow-hidden ml-[-10%]'>
             <img
               src="https://via.placeholder.com/100x100" // Replace with your PFP image URL
               alt="Profile Picture"
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover rounded-full"
             />
-        </div>
+          </div>
+
+          {/* Right section with icon and button */}
+          <div className='flex items-center justify-between w-full '>
+            {/* Icon */}
+            <div className='ml-[35%]'>
+              <a onClick={togglePopup}>
+                <AiOutlineUserAdd color='white' size={50} />
+              </a>
+            </div>
+          </div>
         </div>
 
-          <div>
-            <a onClick={togglePopup}>
-              <AiOutlineUserAdd color='white' size={50} />
-            </a>
-          </div>
-        </div>
-      </div>
-      {isPopupOpen && (
-        <div className='text-white popup h-[70%] w-[30%] bg-mainBlack border-2 rounded-lg opacity-100 z-10 mt-[-40%] ml-[-60%]'>
-          <div className='popup-content opacity-100 z-20 flex flex-col items-center justify-center'>
-            <h2 className='text-4xl py-8'>Enter Wallet Address</h2>
-            <input
-              type='text'
-              placeholder='Wallet Address'
-              value={walletAddress}
-              className='pl-3 round-lg text-black border-3 border-customGreen border rounded-lg'
-              onChange={handleWalletAddressChange}
-            />
-            <button onClick={setWalletAddress} className='mt-9 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800'>
-              <span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
-                Add friend
-              </span>
-            </button>
-            <button onClick={togglePopup} type='button' className='mt-9 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>Close</button>
-            <div className='relative w-full pb-[43%]'>
-              <iframe src='https://giphy.com/embed/l3fZG4tZuHMzap9N6' className='absolute inset-0 w-full h-full' frameBorder='0' allowFullScreen></iframe>
+        {isPopupOpen && (
+          <div className='text-black  bg-white popup h-[40%] w-[30%] border-2 rounded-lg opacity-100 z-10 mt-[-40%] ml-[-60%]'>
+            <div className='popup-content opacity-100 z-20 flex flex-col items-center justify-center'>
+              <h2 className='text-4xl py-8'>Enter Wallet Address</h2>
+              <input
+                type='text'
+                placeholder='Wallet Address'
+                value={walletAddress}
+                className='pl-3 round-lg text-black border-3 border-customGreen border rounded-lg'
+                onChange={handleWalletAddressChange}
+              />
+              <button onClick={setWalletAddress} className='mt-9 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800'>
+                <span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                  Add friend
+                </span>
+              </button>
+              <button onClick={togglePopup} type='button' className='mt-9 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>Close</button>
+              <div className='relative w-full pb-[43%]'>
+                <iframe src='https://giphy.com/embed/l3fZG4tZuHMzap9N6' className='absolute inset-0 w-full h-full' frameBorder='0' allowFullScreen></iframe>
+              </div>
+              <p><a href='https://giphy.com/gifs/starwars-movie-star-wars-l3fZG4tZuHMzap9N6'>via GIPHY</a></p>
             </div>
-            <p><a href='https://giphy.com/gifs/starwars-movie-star-wars-l3fZG4tZuHMzap9N6'>via GIPHY</a></p>
           </div>
+        )}
+
+        <div>
+          {/* All profiles */}
         </div>
-      )}
-      <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[30%] mr-[60%] h-[50%] mt-[-54%] xl:mt-[-29%]'>
-        {/* USER'S PROFILE */}
-      </div>
-      <div>
-        {/* All profiles */}
       </div>
     </div>
-  )
+  );
 }
 
 export default Chat;
