3931d945defe0283a691bea5f1f16f3744642e18
maushish <chamarnumber102@gmail.com>
2023-09-18 00:01:38 +0530

    Added the functionality of modal

--- front-end/src/Components/Add.jsx
+++ front-end/src/Components/Add.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function Add() {
+
+function Add({visible}) {
+    if (!visible)return null;
   return (
-    <div>Add</div>
+    <div className='fixed inset-0  bg-opacity-100 z-20 backdrop-blur-sm flex justify-center items-center'>
+        <div className='bg-black rounded-xl'>adddd</div>
+    </div>
   )
 }
 
-export default Add
+export default  Add
--- front-end/src/Components/Chat.jsx
+++ front-end/src/Components/Chat.jsx
@@ -20,31 +20,34 @@ function Chat() {
   //   setIsPopupOpen(false);
   // };
 
-  return (
-    <div>
+      const [showModal, hideModal] = useState(false); // Use 'setShowModal' for the updater function
+  
+    return (
       <div>
+        <div>
           <div className="relative min-h-screen flex flex-col bg-mainBlack">
-          <div className='container flex flex-col items-center relative'>
-            <div className='bg-black-gradient border border-gray-300  rounded-xl w-[800px] h-[700px] absolute left-[800px] top-40'>chat</div>
-            <img
-              src="https://via.placeholder.com/100x100" // Replace with your PFP image URL
-              alt="Profile Picture"
-              className="w-20 absolute left-[220px] top-[148px] z-20 object-cover rounded-full"
-            />
-            <div className='bg-customGreen w-40 h-[55px] z-1 rounded-3xl absolute left-[250px] top-40 '>
-            <button className='absolute right-6'>
-              <AiOutlineUserAdd color='white' size={50} />
-            </button>
-
-            <Add/>
-             </div>
-            <div className='bg-black-gradient border border-gray-300 rounded-xl w-[350px] h-[500px] absolute left-[200px] top-80'>profile</div>
+            <div className='container flex flex-col items-center relative'>
+              <div className='bg-black-gradient border border-gray-300 rounded-xl w-[800px] h-[700px] absolute left-[800px] top-40'>chat</div>
+              <img
+                src="https://via.placeholder.com/100x100" // Replace with your PFP image URL
+                alt="Profile Picture"
+                className="w-20 absolute left-[220px] top-[148px] z-20 object-cover rounded-full"
+              />
+              <div className='bg-customGreen w-40 h-[55px] z-1 rounded-3xl absolute left-[250px] top-40 '>
+                <button onClick={() => hideModal(true)} className='absolute right-6' >
+                  <AiOutlineUserAdd color='white' size={50} />
+                </button>
+  
+                <Add visible={showModal} />
+              </div>
+              <div className='bg-black-gradient border border-gray-300 rounded-xl w-[350px] h-[500px] absolute left-[200px] top-80'>profile</div>
+            </div>
           </div>
-
-          </div>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+  
+  
 
 export default Chat;
