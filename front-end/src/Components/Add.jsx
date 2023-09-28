11df36bebcf045357b3200c9cbd4d82e231bd4f4
maushish <chamarnumber102@gmail.com>
2023-09-29 00:27:09 +0530

    added handleAddFriend function to pass

--- front-end/src/Components/Add.jsx
+++ front-end/src/Components/Add.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ani from '../Images/ani.mp4';
 
-function Add({ visible,onClose }) {
+function Add({ visible,onClose,onAddFriend }) {
+  const [walletAddress,setWalletAddress]=useState('')//state to capture the address
+  const handleAddFriend=()=>{
+    onAddFriend(walletAddress)
+    onClose()//close the modal
+  }
+
   if (!visible) return null;
   return (
+
     <div className='fixed inset-0 bg-opacity-100 z-20 backdrop-blur-sm flex flex-col justify-center items-center  '>
       <div className="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
         <svg className="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -21,8 +28,10 @@ function Add({ visible,onClose }) {
 
 
 
-        <input placeholder='Enter wallet address' className='pl-2 border rounded-lg  w-80 h-10 mt-[30%]' />
-        <button onClick={onClose} className="mt-[20%] relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+        <input placeholder='Enter wallet address' className='pl-2 border rounded-lg  w-80 h-10 mt-[30%]'
+        onChange={(e)=>setWalletAddress(e.target.value)}
+        value={walletAddress} />
+        <button onClick={handleAddFriend} className="mt-[20%] relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Add Friend
           </span>
