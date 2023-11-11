172e94ca99140a61cc720b038abd4413b510323b
maushish <chamarnumber102@gmail.com>
2023-11-11 18:56:01 +0530

    Server cconfig

--- back-end/index.js
+++ back-end/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const Moralis = require("moralis").default;
 
 const app = express();
-const port = process.env.REACT_URL
+const port = process.env.PORT || 3001
 
 
 app.use(express.json());
